bbf156bb02ab0de6594087805c09e1fc0c29dd07
Michael Lu <yeskky@gmail.com>
2022-09-28 21:07:31 +0800

    解决拼接字符串溢出问题

--- ch13/programs/ch13_strcat_1.c
+++ ch13/programs/ch13_strcat_1.c
@@ -6,19 +6,19 @@
 #include  <stdio.h>
 #include  <string.h>
 
-char *strcat(char *s1, const char *s2);
+char *my_strcat1(char *s1, const char *s2);
 
 int main() {
     char s1[] = "what about";
     char s2[] = " you today";
-    char *s3 = strcat(s1, s2);
+    char *s3 = my_strcat1(s1, s2);
     puts(s3);
 //    printf("%s", s3);
 
     return 0;
 }
 
-char *strcat(char *s1, const char *s2) {
+char *my_strcat1(char *s1, const char *s2) {
     char *p = s1;
     while (*p != '\0')
         p++;
--- ch13/programs/ch13_strcat_2.c
+++ ch13/programs/ch13_strcat_2.c
@@ -4,18 +4,15 @@
 
 
 #include  <stdio.h>
+#include <string.h>
 
 char *my_strcat(char *s1, const char *s2);
 
 int main() {
-    char s1[] = "what about";
+    char s1[20] = "what about";//s1必须预留 s1+s2 的长度，否则拼接后数组溢出，返回值不正确。
     char s2[] = " you today";
-
-    // char *p4 = my_strcat(s1, s2);  // 这句调用后，结果是不正确的，不知道为什么？
-
-    my_strcat(s1, s2);
-    puts(s1);
-
+    char *p4 = my_strcat(s1, s2);
+    puts(p4);
     return 0;
 }
 
@@ -25,6 +22,5 @@ char *my_strcat(char *s1, const char *s2) {
         p++;
     while ((*p++ = *s2++)) {
     }
-    puts(s1);
     return s1;
 }
